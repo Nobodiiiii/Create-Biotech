9009a15e74cf99a122e9b7b1a8cda1d5af200440
Nobodiiiii <mmu503690@gmail.com>
2026-05-17 00:39:46 +0800

    蜘蛛站

--- src/main/java/com/nobodiiiii/createbiotech/content/spiderassemblytable/SpiderAssemblyTableRenderer.java
+++ src/main/java/com/nobodiiiii/createbiotech/content/spiderassemblytable/SpiderAssemblyTableRenderer.java
@@ -41,7 +41,7 @@ public class SpiderAssemblyTableRenderer extends KineticBlockEntityRenderer<Spid
 		new ResourceLocation("minecraft", "textures/entity/spider/spider.png");
 	private static final float SPIDER_SCALE = 1.0f;
 	private static final float SPIDER_Y_OFFSET = 0.5f + 15f / 16f * SPIDER_SCALE;
-	private static final float ACTIVE_LEG_BEND = (float) Math.toRadians(20);
+	private static final float ACTIVE_LEG_BEND = (float) Math.toRadians(45);
 	private static final float SHAFT_OFFSET = 0.34f;
 	private static final float MACHINE_SCALE = 0.4f;
 	private static final float LEG_LENGTH_MODEL = 15.0f;
